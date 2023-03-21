412017bf2124bb0c93b327c5763d610d0dd09b8f
Seung183 <kimsy970911@naver.com>
2023-03-21 16:38:58 +0900

    BOJ

---
+++ 2.조건문/2.1(#1330)_두 수 비교/Main.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String num = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(num);
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if(a > b){
+            bw.write(">");
+        }else if( a < b ){
+            bw.write("<");
+        }else{
+            bw.write("==");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
